65c83915e3aaeac8cf94c7253e6a64b0c577751f
RusiruJayanga <rusirujayanga@gmail.com>
2025-02-25 22:58:19 +0530

    Front-end 4
    Navigation bar is continue.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,13 +4,16 @@ import { Route, Routes } from "react-router-dom";
 import Footer from "./components/user/common/footer/Footer";
 import Navigation_bar_head from "./components/user/customer/navigation/Navigation_bar_head";
 import Navigation_bar from "./components/user/customer/navigation/Navigation_bar";
+//pages
+//customer pages
+import Home from "./pages/user/customer/home/Home";
 
 const App = () => {
   return (
     <div>
       <Navigation_bar_head />
       <Navigation_bar />
-      <Footer />
+      <Home />
     </div>
   );
 };
