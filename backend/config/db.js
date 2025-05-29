19694c6caaa42edbcf76b23411d01b97fac8bda7
Rusiru Jayanga <132242402+RusiruJayanga@users.noreply.github.com>
2025-05-29 20:47:54 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   await mongoose
     .connect(
-      `mongodb+srv://rusirujayanga:VyREmbcSeP3iovQg@cluster0.qapfzhc.mongodb.net/E-pharmacy-platform`
+      ``
     )
     .then(() => console.log("DB Connected"));
 };
